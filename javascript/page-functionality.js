9867caf71d361009d28fc3abcfaf92248b7f5566
skeller88 <skeller88@gmail.com>
2014-05-29 16:49:02 -0700

    Add vertically centered header

--- javascript/page-functionality.js
+++ javascript/page-functionality.js
@@ -1,43 +1,57 @@
 $(function(){
-    //show and hide different 'pages' based on the link clicked
-    $('.blog-link').on('click', function(){
+    //event handlers
+    var showBlog = function(){
         $('.container > section').hide();
         $('#blog').css('display', 'inline-block');
         $('#blog > article').show();
-    }.bind(this));
+    }.bind(this);
 
-    $('.about-link').on('click', function(){
+    var showAbout = function(){
         $('.container > section').hide();
         $('#about').css('display', 'inline-block');
-    }.bind(this));
+    }.bind(this);
 
-    $('.classes-link').on('click', function(){
+    var showClasses = function(){
         $('.container > section').hide();
         $('#classes').css('display', 'inline-block');
-    }.bind(this));
-
-    //show and hide different blog posts 
-    $('#sidebar li').on('click', function(evt){
-        var href = $(evt.target).attr('href');
+    }.bind(this);
+    
+    var followLink = function(evt){
+        var el = $(evt.target);
+        if(el.prop('tagName') === 'A'){
+            var articleId = el.attr('href');
+        }else{
+            var articleId = el.children(0).attr('href');
+        }
         $('.container > section').hide();
         $('#blog').css('display', 'inline-block');
         $('#blog > article').hide();
-        $(href).show();
-    }.bind(this));
+        $(articleId).show();
+    }.bind(this);
 
-    //click handlers
-
-    $('button').on('click', function(){
+    var fakeServerReq = function(){
         $('button').hide();
         $('#jquery-msg').show();
         console.log('<button> clicked');
         console.log('if this app had a server, information indicating that the blog post had been "liked" would be sent to the server right now.');
-    }.bind(this));
+    }.bind(this);
 
-    $('#jquery-msg').on('click', function(){
+    var hideJQueryMsg = function(){
         $('#jquery-msg').hide();
         $('button').show();
         console.log('<p> with id "jquery-msg" clicked');
-        console.log('if this app had a server, information indicating that the blog post had been "liked" would be sent to the server right now.');
-    }.bind(this));
+    }.bind(this);
+
+    //show and hide different 'pages' based on the link clicked
+    $('.blog-link').on('click', showBlog);
+    $('.about-link').on('click', showAbout);
+    $('.classes-link').on('click', showClasses);
+
+    //show and hide different blog posts 
+    $('#sidebar li').on('click', followLink);
+    $('article h2').on('click', followLink);
+
+    //show and hide jQuery message
+    $('button').on('click', fakeServerReq);
+    $('#jquery-msg').on('click', hideJQueryMsg);
 })
