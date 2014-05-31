ca2f97ab0bf337f8a5f8f785150506f694bf06b6
skeller88 <skeller88@gmail.com>
2014-05-31 01:32:09 -0700

    Finish blog entry preview functionality

--- javascript/page-functionality.js
+++ javascript/page-functionality.js
@@ -26,16 +26,21 @@ $(function(){
         if(el.prop('tagName') === 'A'){
             var articleId = el.attr('href');
         }else if(el.prop('tagName') === 'SPAN'){
-            el.hide();
-            el.parent().siblings('p').show();
-            var articleId = el.parent().siblings('h2').children(0).attr('href');
-        }else{
+            var articleId = el.siblings('h2').children(0).attr('href');
+        }else if(el.prop('tagName') === 'LI'){
             var articleId = el.children(0).attr('href');
         }
+        var $articleId = $(articleId);
+
+        //hide all content 
         $('.container > section').hide();
-        $('#blog').css('display', 'inline-block');
         $('#blog > article').hide();
-        $(articleId).show();
+        $articleId.children('.read-more').hide();
+
+        //show only the content for the article clicked on 
+        $articleId.show();
+        $('#blog').css('display', 'inline-block');
+        $articleId.children('p').show();
     }.bind(this);
 
     var fakeServerReq = function(){
@@ -59,7 +64,7 @@ $(function(){
     //go to a 'page' containing a single blog post
     $('#sidebar li').on('click', followLink);
     $('article h2').on('click', followLink);
-    $('article .preview').on('click', followLink);
+    $('article .read-more').on('click', followLink);
 
     //show and hide jQuery message
     $('button').on('click', fakeServerReq);
